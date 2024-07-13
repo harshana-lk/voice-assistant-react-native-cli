4063733315053cc17baab18d9be80507f15c3cf9
harshana-lk <harshuonlive@gmail.com>
2024-07-13 20:27:26 +0530

    Successfully configured the application

---
+++ App.js
@@ -0,0 +1,8 @@
+import { View, Text, SafeAreaView } from 'react-native'
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello React-Native</Text>
+    </SafeAreaView>
+  )
+}
