42ef8f523642835d12364bb01a3f936de735ae67
harshana-lk <harshuonlive@gmail.com>
2024-07-14 11:44:26 +0530

    Added & Confiured the React navigations to the project

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { Text, SafeAreaView } from 'react-native'
+import AppNavigation from "./src/navigation"
 export default function App() {
   return (
-    <SafeAreaView className="bg-green-200">
-      <Text className="text-3xl font-bold">Hello React-Native</Text>
-    </SafeAreaView>
+    <AppNavigation/>
   )
 }
